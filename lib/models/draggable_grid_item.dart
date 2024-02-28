850e7c0ca5a3344625c17ab1be3b075f30afb1a6
Michael2543 <Michael@Sellmaiernet.de>
2024-02-28 12:14:50 +0000

    undone index

--- lib/models/draggable_grid_item.dart
+++ lib/models/draggable_grid_item.dart
@@ -4,13 +4,12 @@ part of draggable_grid_view;
 /// [child] will show the widgets in Gridview.builder.
 /// [isDraggable] is boolean, you want to allow dragging then set it true or else false.
 class DraggableGridItem {
-  DraggableGridItem(
-      {required this.child,
-      this.isDraggable = false,
-      this.dragCallback,
-      this.index = 0});
+  DraggableGridItem({
+    required this.child,
+    this.isDraggable = false,
+    this.dragCallback,
+  });
 
-  final int index;
   final bool isDraggable;
   final Widget child;
   final Function(BuildContext context, bool isDragging)? dragCallback;
